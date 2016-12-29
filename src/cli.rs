a99c6bb2f79c75930fc2ab5c0b80b5284065e5bc
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-12-29 01:39:34 +0100

    Show the help when executed without arguments

--- src/cli.rs
+++ src/cli.rs
@@ -5,6 +5,7 @@ pub fn parse_cli<'a>() -> ArgMatches<'a> {
         .version(crate_version!())
         .setting(AppSettings::SubcommandsNegateReqs)
         .setting(AppSettings::VersionlessSubcommands)
+        .setting(AppSettings::ArgRequiredElseHelp)
         .set_term_width(80)
         .usage("rconc <server> <command>...\n    \
                 rconc server add <name> <address> <password>\n    \
